3abfd566d555315e699db9405ec348bb07fa94be
reduan2660 <alve.reduan.09@gmail.com>
2022-08-04 20:59:28 +0600

    Spring Setup

---
+++ backend/src/test/java/com/ytvideoshare/backend/YoutubeVideoSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytvideoshare.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeVideoSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
