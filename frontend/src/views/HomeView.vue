4e447803f1f2201032d4c4a6a54246ac0de07c66
reduan2660 <alve.reduan.09@gmail.com>
2022-08-05 15:07:50 +0600

    Hello from Tailwind

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="font-normal hover:font-bold">Hello from TailwindCSS</h1>
   </main>
 </template>
