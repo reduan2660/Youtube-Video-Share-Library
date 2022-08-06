01ea9d01ca7d61f15f38cb82d5572b4b867207f9
reduan2660 <alve.reduan.09@gmail.com>
2022-08-06 18:47:34 +0600

    CORS fix

--- backend/src/main/java/com/ytvideoshare/backend/filter/CORSfilter.java
+++ backend/src/main/java/com/ytvideoshare/backend/filter/CORSfilter.java
@@ -28,11 +28,11 @@ public class CORSfilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, AUTHORIZATION");
 
         chain.doFilter(req, res);
     }
